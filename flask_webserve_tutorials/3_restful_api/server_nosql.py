87cd6bc9888508c34970b7b62564e055ed2ea61c
Danny Young <git@youngshome.com>
2017-11-07 01:48:00 -0800

    fixed problem with no row having both 'country' and 'born_in'.  Also started restful api tutorial

---
+++ flask_webserve_tutorials/3_restful_api/server_nosql.py
@@ -0,0 +1,24 @@
+# server_nosql.py
+import flask
+import pymongo
+import bson.json_util
+
+app = flask.Flask(__name__)
+
+db = pymongo.MongoClient().nobel_prize
+
+@app.route('/api/winners')
+def get_country_data():
+    query_dict = {}
+    for key in ['country', 'category', 'year']:
+        arg = pymongo.request.args.get(key) # gives args of request ('?country=Australia&categor=Chemistry')
+        if arg:
+            query_dict[key] = arg
+    winners = db.winners_clean.find(query_dict)
+    if winners:
+        return bson.json_util.dumps(winners) # this dumps can serialize date objects
+    flask.abort(404) # resource not found
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
+
