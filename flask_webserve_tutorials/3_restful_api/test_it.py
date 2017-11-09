e19795886a25097f6e4df903a9382c669a3ab35b
Danny Young <git@youngshome.com>
2017-11-09 00:49:18 -0800

    small change to restful_api test to see if it is working -- still not, but getting 200 response now

--- flask_webserve_tutorials/3_restful_api/test_it.py
+++ flask_webserve_tutorials/3_restful_api/test_it.py
@@ -1,5 +1,7 @@
 import requests
 
 response = requests.get('http://localhost:8000/api/winners', params={'country':'Australia'})
+print(response.json())
+response = requests.get('http://localhost:8000/api/winners', params={})
+print(response.json())
 
-response.json()
